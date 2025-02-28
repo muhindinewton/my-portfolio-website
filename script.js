edb4102fbcce7207561c798fb16a7b3da6f10aa2
Newton Muhindi <muhindinewton@gmail.com>
2025-03-01 00:07:04 +0300

    set menu appearance to hamburger for mobile devices, and linked script.js to html

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("show");
+}
